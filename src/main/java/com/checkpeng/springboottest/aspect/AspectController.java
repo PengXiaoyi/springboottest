b38d5b44e8ba9e85ab2188796313027c679f1198
彭潇懿80280648 <checkpeng@cmbchina.com>
2020-11-23 20:23:25 +0800

    test

--- src/main/java/com/checkpeng/springboottest/aspect/AspectController.java
+++ src/main/java/com/checkpeng/springboottest/aspect/AspectController.java
@@ -1,6 +1,5 @@
 package com.checkpeng.springboottest.aspect;
 
-import com.sun.org.slf4j.internal.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
